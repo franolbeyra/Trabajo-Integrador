1db2220a53fd0746686d73b2b9f958e417db82cc
Franco Olbeyra <fran-olbeyra@hotmail.com>
2024-10-17 21:33:16 -0300

    funcion cambiar nota

--- funcion_cambiarnota.js
+++ funcion_cambiarnota.js
@@ -1,13 +1,45 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
+import estudiantesNotas from './arraynotasyasistencia.js';
 
 
-function CambiarNota() {
-   let nombre = prompt("Ingrese su nombre: ");
-   let materia = prompt("Ingrese la materia: ");
+function cambiarNotaPorNombre() {
+   let nombre = prompt("Ingrese el nombre del estudiante: ");
+   let apellido=prompt("Ingrese el apellido del estudiante: ");
+   let materia=prompt("Ingrese el nombre de la materia: ");
    
-   console.log("Su nombre es: " + nombre);
-   console.log("La materia es: " + materia);
+
+   
+   // Buscar al estudiante por nombre y apellido
+   const estudiante = estudiantesNotas.find(est => est.nombre === nombre && est.apellido === apellido);
+   
+   if (!estudiante) {
+       console.log("Estudiante no encontrado.");
+       return;
+   }
+
+   // Buscar el examen de la materia especificada
+   const examen = estudiante.examenes.find(ex => ex.materia === materia);
+   
+   if (!examen) {
+       console.log("Materia no encontrada.");
+       return;
+   }
+   console.log(`Las notas actuales de ${nombre} en la materia ${materia} son: ${examen.notas}`);
+      let indiceNota=  Number(prompt("Ingrese el indice de la nota que quiere agregar sabiendo que indice en 0 es la primer nota: "));
+      let nuevaNota=  Number(prompt("Ingrese la nueva nota: "));
+   // Validar si el índice de la nota es válido
+   if (indiceNota < 0 || indiceNota >= examen.notas.length) {
+       console.log("Índice de nota inválido.");
+       return;
+   }
+
+   // Cambiar la nota
+   examen.notas[indiceNota] = nuevaNota;
+   console.log(`Nota cambiada a ${nuevaNota} en la materia ${materia} para ${nombre} ${apellido}`);
+   console.log(`Las notas actualizadas para  ${nombre} ${apellido} en la materia ${materia} ${examen.notas}`);
 }
 
-CambiarNota();
+// Ejemplo de uso:
+//cambiarNotaPorNombre("Carlos", "Fernandez", "programacion", 1, 9); 
+cambiarNotaPorNombre()
